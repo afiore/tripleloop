23db7fd394744500de0b7f31e48e49bab02226c6
Andrea Fiore <andrea.giulio.fiore@gmail.com>
2013-02-25 16:31:59 +0000

    Minor refactoring

--- example/lib/extractors/authors_extractor.rb
+++ example/lib/extractors/authors_extractor.rb
@@ -4,25 +4,25 @@ class AuthorsExtractor < Tripleloop::Extractor
   bind(:doi)      { |doc| RDF::DOI.send(doc['doi']) }
   bind(:graph)    { |_|   RDF::NPGG.authors }
 
-  map('authors') { |authors|
-    article_authors = authors.map {|a|
-      [doi, RDF::NPG.hasAuthor, author_uri(a['full']), graph]
-    }
-
-    affiliations = authors.reduce([]) { |accu, author|
-      accu + author.fetch('affiliations', []).map { |affiliation|
-        [author_uri(author['full']),
-         RDF::NPG.affiliatedTo,
-         institution_uri(affiliation),
-         graph]
-      }
-    }
-
-    article_authors + affiliations
+  map('authors') { |authors_data|
+    article_authors(authors_data) + affiliations(authors_data)
   }
 
-
 private
+  def article_authors(authors)
+    authors.map {|a|
+      [doi, RDF::NPG.hasAuthor, author_uri(a['full']), graph]
+    }
+  end
+
+  def affiliations(authors)
+    authors.reduce([]) { |accu, author|
+      accu + author.fetch('affiliations', []).map { |affiliation|
+        [author_uri(author['full']), RDF::NPG.affiliatedTo, institution_uri(affiliation), graph]
+      }
+    }
+  end
+
   def author_uri(full_name)
     uri = "http://ns.nature.com/institutions/{name}"
     RDF::URI.new template_uri(uri).expand(:name => full_name)
